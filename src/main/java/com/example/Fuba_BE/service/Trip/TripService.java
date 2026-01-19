e7a4f5fdd9c0f9c2be98b2f0f6ce573f653d1ef7
duyngulam <neargamer3103@gmail.com>
2026-01-20 00:25:05 +0700

    ok nha pns

--- src/main/java/com/example/Fuba_BE/service/Trip/TripService.java
+++ src/main/java/com/example/Fuba_BE/service/Trip/TripService.java
@@ -306,7 +306,7 @@ public class TripService implements ITripService {
         if (departureTime.isBefore(LocalDateTime.now()))
             throw new BadRequestException("Thời gian khởi hành không được ở trong quá khứ!");
 
-        Route route = routeRepository.findById(request.getRouteId())
+        Route route = routeRepository.findByIdWithLocations(request.getRouteId())
                 .orElseThrow(() -> new ResourceNotFoundException("Route not found"));
         Vehicle vehicle = vehicleRepository.findById(request.getVehicleId())
                 .orElseThrow(() -> new ResourceNotFoundException("Vehicle not found"));
